3333cc00f8c6ecaa93df05114da55dffcd4488ab
Edwin van der Ven <edwinvanderven@gmail.com>
2019-04-27 15:05:43 +0200

    Added initial inputs for selecting a color and a color identifier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'color-schemer';
+export class AppComponent implements OnInit {
+  @ViewChild('colorInput') colorInput: ElementRef;
+
+  public title = 'color-schemer';
+  public color: string = '000000';
+
+  private LOCAL_STORAGE_COLOR: string = 'color-schemer-set-color';
+
+  constructor() {
+    this.color = localStorage.getItem(this.LOCAL_STORAGE_COLOR) || '000000';
+  }
+
+  ngOnInit() {
+    this.colorInput.nativeElement.focus();
+  }
+
+
+  onColorChange(value: string) {
+    this.color = value;
+    localStorage.setItem(this.LOCAL_STORAGE_COLOR, this.color)
+  }
+
+  
+  colorToHex() : string {
+    return '#' + this.color;
+  }
 }
